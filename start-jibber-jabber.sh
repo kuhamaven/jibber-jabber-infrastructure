b52185917d0206a1ce85df032e946d60dddefa23
khalil <khalil.stessens@ing.austral.edu.ar>
2021-07-08 15:49:13 -0300

    Updated Starting Script

--- start-jibber-jabber.sh
+++ start-jibber-jabber.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
 YELLOW=$'\e[1;33m'
 PURPLE=$'\e[1;35m'
 CYAN=$'\e[1;36m'
 NC=$'\e[0m'
+
+pause() {
+  echo -n "${PURPLE}Press Q when you want to stop Jibber Jabber... "
+  while true; do
+    read -n1 -r
+    [[ $REPLY == 'q' ]] && break
+  done
+  echo
+}
+
 echo "${PURPLE}Starting Jibber Jabber!${NC}"
 echo "${YELLOW}Pulling latest images for all services!${NC}"
 docker-compose pull -q
@@ -10,5 +21,8 @@ echo "${YELLOW}That's it! Everything up to date!${NC}"
 echo "${CYAN}Building nginx!${NC}"
 docker-compose build -q
 echo "${CYAN}Okay, ready to roll!${NC}"
-docker-compose up --force-recreate
+docker-compose up --force-recreate -d
+echo "${CYAN}Jibber Jabber is now running!${NC}"
+pause
+docker-compose down
 echo "${PURPLE}Thanks! Have a nice day!${NC}"
