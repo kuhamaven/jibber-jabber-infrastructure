1d76113d0ed2d5b0faff6ea92f1c2db5aeeb1fd3
Sofia Sidañez <sofia.sidanez@ing.austral.edu.ar>
2021-07-10 12:31:16 -0300

    Splitted into Dev and Prod

--- start-jibber-jabber-dev.sh
+++ start-jibber-jabber-dev.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
 YELLOW=$'\e[1;33m'
+RED=$'\e[0;31m'
 PURPLE=$'\e[1;35m'
 CYAN=$'\e[1;36m'
 NC=$'\e[0m'
 
-pause() {
-  echo -n "${PURPLE}Press Q when you want to stop Jibber Jabber... "
-  while true; do
-    read -n1 -r
-    [[ $REPLY == 'q' ]] && break
-  done
-  echo
-}
-
+docker-compose -f docker-compose.dev.yml down
 echo "${PURPLE}Starting Jibber Jabber!${NC}"
+echo "${RED}Current profile: Dev${NC}"
 echo "${YELLOW}Pulling latest images for all services!${NC}"
-docker-compose pull -q
+docker-compose -f docker-compose.dev.yml pull -q
 echo "${YELLOW}That's it! Everything up to date!${NC}"
 echo "${CYAN}Building nginx!${NC}"
-docker-compose build -q
+docker-compose -f docker-compose.dev.yml build -q
 echo "${CYAN}Okay, ready to roll!${NC}"
-docker-compose up --force-recreate -d
+docker-compose -f docker-compose.dev.yml up --force-recreate -d
 echo "${CYAN}Jibber Jabber is now running!${NC}"
-pause
-docker-compose down
 echo "${PURPLE}Thanks! Have a nice day!${NC}"
---
+++ start-jibber-jabber-prod.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+YELLOW=$'\e[1;33m'
+RED=$'\e[0;31m'
+PURPLE=$'\e[1;35m'
+CYAN=$'\e[1;36m'
+NC=$'\e[0m'
+
+docker-compose -f docker-compose.prod.yml down
+echo "${PURPLE}Starting Jibber Jabber!${NC}"
+echo "${RED}Current profile: Prod${NC}"
+echo "${YELLOW}Pulling latest images for all services!${NC}"
+docker-compose -f docker-compose.prod.yml pull -q
+echo "${YELLOW}That's it! Everything up to date!${NC}"
+echo "${CYAN}Building nginx!${NC}"
+docker-compose -f docker-compose.prod.yml build -q
+echo "${CYAN}Okay, ready to roll!${NC}"
+docker-compose -f docker-compose.prod.yml up --force-recreate -d
+echo "${CYAN}Jibber Jabber is now running!${NC}"
+echo "${PURPLE}Thanks! Have a nice day!${NC}"
