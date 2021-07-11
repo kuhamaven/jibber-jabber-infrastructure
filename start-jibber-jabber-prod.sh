e7a152b44131245218f68b6ef26a2f882a6280cf
khalil <khalil.stessens@ing.austral.edu.ar>
2021-07-11 19:43:27 -0300

    fixed prod compose

--- start-jibber-jabber-prod.sh
+++ start-jibber-jabber-prod.sh
@@ -7,16 +7,16 @@ CYAN=$'\e[1;36m'
 NC=$'\e[0m'
 
 cd ~/jibber-jabber-infrastructure/
-docker-compose -f ~/jibber-jabber-infrastructure/docker-compose.prod.yml down
+docker-compose -f ./docker-compose.prod.yml down
 echo "${PURPLE}Starting Jibber Jabber!${NC}"
 echo "${RED}Current profile: Prod${NC}"
 echo "${YELLOW}Pulling latest images for all services!${NC}"
-docker-compose -f ~/jibber-jabber-infrastructure/docker-compose.prod.yml pull -q
+docker-compose -f ./docker-compose.prod.yml pull -q
 echo "${YELLOW}That's it! Everything up to date!${NC}"
 echo "${CYAN}Building nginx!${NC}"
-docker-compose -f ~/jibber-jabber-infrastructure/docker-compose.prod.yml build -q
+docker-compose -f ./docker-compose.prod.yml build -q
 echo "${CYAN}Okay, ready to roll!${NC}"
-docker-compose -f ~/jibber-jabber-infrastructure/docker-compose.prod.yml up --force-recreate -d
+docker-compose -f ./docker-compose.prod.yml up --force-recreate -d
 echo "${CYAN}Jibber Jabber is now running!${NC}"
 echo "${PURPLE}Thanks! Have a nice day!${NC}"
-~/jibber-jabber-infrastructure/init-letsencrypt.prod.sh
+./init-letsencrypt.prod.sh
