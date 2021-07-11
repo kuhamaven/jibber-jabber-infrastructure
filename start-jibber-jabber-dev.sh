caac94c72891bfdf134fd567d2fb2b85af07f285
khalil <khalil.stessens@ing.austral.edu.ar>
2021-07-11 15:51:47 -0300

    changed scripts

--- start-jibber-jabber-dev.sh
+++ start-jibber-jabber-dev.sh
@@ -7,16 +7,16 @@ CYAN=$'\e[1;36m'
 NC=$'\e[0m'
 
 cd ~/jj/jibber-jabber-infrastructure/
-docker-compose -f docker-compose.dev.yml down
+docker-compose -f ~/jj/jibber-jabber-infrastructure/docker-compose.dev.yml down
 echo "${PURPLE}Starting Jibber Jabber!${NC}"
 echo "${RED}Current profile: Dev${NC}"
 echo "${YELLOW}Pulling latest images for all services!${NC}"
-docker-compose -f docker-compose.dev.yml pull -q
+docker-compose -f ~/jj/jibber-jabber-infrastructure/docker-compose.dev.yml pull -q
 echo "${YELLOW}That's it! Everything up to date!${NC}"
 echo "${CYAN}Building nginx!${NC}"
-docker-compose -f docker-compose.dev.yml build -q
+docker-compose -f ~/jj/jibber-jabber-infrastructure/docker-compose.dev.yml build -q
 echo "${CYAN}Okay, ready to roll!${NC}"
-docker-compose -f docker-compose.dev.yml up --force-recreate -d
+docker-compose -f ~/jj/jibber-jabber-infrastructure/docker-compose.dev.yml up --force-recreate -d
 echo "${CYAN}Jibber Jabber is now running!${NC}"
 echo "${PURPLE}Thanks! Have a nice day!${NC}"
-./init-letsencrypt.dev
+~/jj/jibber-jabber-infrastructure/init-letsencrypt.dev.sh
